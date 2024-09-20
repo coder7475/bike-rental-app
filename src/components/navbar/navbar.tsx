61e4f87547d70cca66f33cd7d718b6f3f1329fbb
coder7475 <robiulhossain7475@gmail.com>
2024-09-20 20:30:11 +0600

    feat: change icon and name in nvabar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -8,19 +8,12 @@ const Navbar = () => {
           to="/"
           className="mb-4 flex items-center font-medium text-white md:mb-0"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="size-10 rounded-full bg-indigo-500 p-2 text-white"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <span className="ml-3 text-xl">Tailblocks</span>
+          <img
+            src="/bike.png"
+            alt="cyclone"
+            className="size-10 rounded-xl bg-card"
+          />
+          <span className="ml-3 text-xl">Cyclone</span>
         </Link>
         <nav className="flex flex-wrap items-center justify-center text-base md:ml-4	md:mr-auto md:border-l md:border-gray-700 md:py-1 md:pl-4">
           <Link to="/" className="mr-5 hover:text-white">
