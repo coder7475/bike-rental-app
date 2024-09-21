7ab53ef6386e58bb2ea302240c01d50a7cab940b
coder7475 <robiulhossain7475@gmail.com>
2024-09-21 22:01:22 +0600

    feat: create a empty banner

--- src/app/routes/landing.tsx
+++ src/app/routes/landing.tsx
@@ -1,8 +1,7 @@
-import logo from '@/assets/logo.svg';
+import Banner from '@/components/banner/banner';
 import Footer from '@/components/footer/footer';
 import Navbar from '@/components/navbar/navbar';
 import { Head } from '@/components/seo';
-import { Button } from '@/components/ui/button';
 
 export const LandingRoute = () => {
   return (
@@ -10,36 +9,7 @@ export const LandingRoute = () => {
       <Navbar />
       <Head description="Cyclone - Bike Rental Website" />
       <div className="flex h-screen items-center bg-white">
-        <div className="mx-auto max-w-7xl px-4 py-12 text-center sm:px-6 lg:px-8 lg:py-16">
-          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            <span className="block">Cyclone</span>
-          </h2>
-          <img src={logo} alt="react" />
-          <p>Showcasing Best Practices For Building React Applications</p>
-          <div className="mt-8 flex justify-center">
-            <Button
-              onClick={() => console.log('Get started clicked')}
-              icon={
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                  />
-                </svg>
-              }
-            >
-              Get started
-            </Button>
-          </div>
-        </div>
+        <Banner />
       </div>
       <Footer />
     </>
