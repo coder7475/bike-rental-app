83757798af9c6e5bd4b62aa89b5bd07427a79c55
coder7475 <robiulhossain7475@gmail.com>
2024-09-24 22:10:01 +0600

    feat: adjust the banner colors and paddings

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -5,15 +5,17 @@ const Banner = () => {
 
   return (
     <div className="relative size-full">
+      {/* Backgrond Image  */}
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{ backgroundImage: "url('/bike-banner.jpg')" }}
       >
         <div className="absolute inset-0 bg-black/50" />
       </div>
+      {/* Main Text  */}
       <div className="relative flex h-full flex-col items-center justify-center space-y-6 px-4 text-center text-white sm:space-y-8">
         <h1 className="text-3xl font-bold sm:text-4xl md:text-5xl lg:text-6xl">
-          Explore the City on Two Wheels
+          Explore Above and Beyond
         </h1>
         <p className="text-lg sm:text-xl md:text-2xl">
           Rent a bike and start your adventure today!
@@ -28,14 +30,14 @@ const Banner = () => {
               className="w-full rounded-full border-2 border-white bg-transparent px-4 py-2 text-white placeholder:text-white sm:rounded-r-none"
               placeholder="Check availability"
             />
-            <button className="mt-2 w-full rounded-full bg-card-foreground px-6 py-2 font-semibold transition duration-300 hover:bg-green-600 sm:mt-0 sm:w-auto sm:rounded-l-none">
+            <button className="mt-2 w-full rounded-full bg-card-foreground px-4 py-2 font-semibold transition duration-300 hover:bg-primary sm:mt-0 sm:w-auto sm:rounded-l-none">
               Search
             </button>
           </div>
         </div>
         <a
           href="#rent"
-          className="rounded-full bg-primary px-6 py-2 text-lg font-semibold transition duration-300 hover:bg-blue-700 sm:px-8 sm:py-3"
+          className="rounded-full bg-primary px-6 py-2 text-lg font-semibold transition duration-300 hover:bg-primary sm:px-8 sm:py-3"
         >
           Rent Now
         </a>
